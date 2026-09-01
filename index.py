bc9f58196aa32c03d56a65409d4c7d7c5d7dafbc
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-08-31 22:15:10 -0600

    add First class

---
+++ index.py
@@ -0,0 +1,33 @@
+cantidadDeVecesEnLaFrendZone = 10
+teQuiere = True
+cuantoDineroTengo = 15400.2
+mi_mejor_amigo_se_llama = "John Wick, dios lo tenga en su sanata gloria"
+variable_no_especificada = None
+peliculas_del_inge = ["La noche del demonio", "Sony"]
+diccionario = {
+    "xbox": "Gears",
+    "Play": "The last of us",
+    "Nintento": ["Zelda","Mario"]
+}
+
+print("Variable cantidadDeVecesEnLaFrendZone")
+print(type(cantidadDeVecesEnLaFrendZone))
+print(cantidadDeVecesEnLaFrendZone)
+print("Variable teQuiere")
+print(teQuiere)
+print(type(teQuiere))
+print("Variable cuantoDineroTengo")
+print(cuantoDineroTengo)
+print(type(cuantoDineroTengo))
+print("Variable mi_mejor_amigo_se_llama")
+print(mi_mejor_amigo_se_llama)
+print(type(mi_mejor_amigo_se_llama))
+print("Variable variable_no_especificada")
+print(variable_no_especificada)
+print(type(variable_no_especificada))
+print("Variable peliculas_del_inge")
+print(peliculas_del_inge)
+print(type(peliculas_del_inge))
+print("Variable diccionario")
+print(diccionario)
+print(type(diccionario))
