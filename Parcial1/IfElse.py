a392f32cdaf8a402c3c70bfff1aed95deebb0782
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-09-04 18:23:32 -0600

    feat: Parcial if | se vio todos los ifs

---
+++ Parcial1/IfElse.py
@@ -0,0 +1,12 @@
+tarimas_de_la_planta = 100
+productos = 200
+cantidad_productos_portarima = 20
+
+print(tarimas_de_la_planta)
+print(productos)
+print(cantidad_productos_portarima)
+
+if productos > 50000:
+    print("Se necesitan más tarimas")
+else:
+    print("No se necesitan más tarimas")
---
+++ Parcial1/index.py
@@ -0,0 +1,40 @@
+tipo_persona = input("Ingrese tipo persona: ")
+
+if(tipo_persona == "estudiante"):
+     print("Bienvenido lincito")
+     avance = int(input("Ingrese su avance: "))
+     print("Su avance es: ",avance)
+     print(type(avance))
+     # and == &&
+     # or == ||
+     cantidad_materias = 10
+     if(avance >=80 or cantidad_materias == 0):
+         print("Bienvenido a residencias")
+
+elif(tipo_persona == "docente"):
+    print("Bienvenido docente")
+else:
+    print("Ni si quiera se quien eres :() ")
+
+
+# if(tipo_persona == "estudiante"):
+#      print("Bienvenido lincito")
+# elif(tipo_persona == "docente"):
+#     print("Bienvenido docente")
+# else:
+#     print("Ni si quiera se quien eres :() ")
+print("-------------------------------")
+# if(tipo_persona == "estudiante"):
+#      print("Bienvenido lincito")
+# else:
+#     print("Docente, invitados, latiga,sugar ")
+
+
+print("-------------------------------")
+
+
+# if(tipo_persona == "estudiante"):
+#     print("Bienvenido lincito")
+    
+
+# print(5 > 2)
