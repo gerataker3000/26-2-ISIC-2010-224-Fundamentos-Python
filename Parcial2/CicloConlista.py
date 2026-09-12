621733d15d231b8a2f2bcbff61a372cbaf6a292d
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-09-11 18:48:10 -0600

    feat: Parical 2 ciclos

---
+++ Parcial2/CicloConlista.py
@@ -0,0 +1,11 @@
+peliculaChidasQueMeGustas = ["Spider-Man", "Avengers", "Iron Man", "Thor"]
+peliculaChidasQueMeGustasTeror = ["Detah Note", "Solo Leveling", "Los 7 Pecados Capitales", "JOJO Bizarre Adventure"]
+
+print(peliculaChidasQueMeGustas[0])
+print(peliculaChidasQueMeGustas[1])
+print(peliculaChidasQueMeGustas[2])
+print(peliculaChidasQueMeGustas[3])
+print("Fin del ciclo de peliculas chidas que me gustan")
+
+for pelicula in peliculaChidasQueMeGustasTeror:
+    print(pelicula)
